6640c1825a750a21eedef165047933541d5e0aea
luisgallegos23 <82685610+luisgallegos23@users.noreply.github.com>
2022-07-06 13:08:29 -0500

    Easy Problem Done

--- challenge.js
+++ challenge.js
@@ -7,7 +7,13 @@ console.log(`The difference between ${num1} and ${num2} is ${num2-num1}.`)
 // EASY: Create two variables and assign a person’s name to each. 
 // Console log a statement that says which name is shorter or longer, and by how many characters. 
 // Example output: “The name Thomas is longer than John by 2 characters”
-
+var name1 = "Joseph";
+var name2 = "Fredrick";
+if(name1.length > name2.length){
+    console.log(`The name ${name1} is longer than ${name2} by ${name1.length - name2.length}.`);
+}else{
+    console.log(`The name ${name2} is longer than ${name1} by ${name2.length - name1.length}.`);
+}
 // MEDIUM: Write a program to tell if someone is shouting (typing in all caps), whispering 
 // (typing in all lowercase), or neither. 
 // Use prompt to get user input, and then console log whether the user was shouting, whispering, 
