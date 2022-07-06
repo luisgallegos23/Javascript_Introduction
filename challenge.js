bb5c59d27b16a994faae8869b7e09b90913a66bc
luisgallegos23 <82685610+luisgallegos23@users.noreply.github.com>
2022-07-06 14:58:22 -0500

    Finished

--- challenge.js
+++ challenge.js
@@ -18,11 +18,51 @@ if(name1.length > name2.length){
 // (typing in all lowercase), or neither. 
 // Use prompt to get user input, and then console log whether the user was shouting, whispering, 
 // or talking normally.
-
+/*var wordIn = prompt("Please type something: ");
+if(wordIn == wordIn.toUpperCase()){
+    alert("WHY ARE YOU SHOUTING!?");
+}else if (wordIn == wordIn.toLowerCase()){
+    alert("why are you wispering?");
+}else{
+    alert("Thank you for talking normally.");
+}*/
 // HARD: Create 4 math functions, one called add() that adds 2 numbers, one c
 // alled subtract() that subtracts 2 numbers, one called multiply() to multiply 2 numbers, 
 // and one called divide() to divide two numbers.
+function add(num1,num2){
+    return num1 + num2;
+}
+
+function subtract(num1,num2){
+    return num1-num2;
+}
+
+function multiply(num1,num2){
+    return num1 * num2;
+}
+
+function divide(num1,num2){
+    return num1/num2;
+}
 
 // VERY HARD: Create a simple calculator that prompts the user for a number, 
 // an operator (either +, -, * or /), and another number, and then uses the functions created 
 // in the hard challenge to output the value in sentence form. Example output: "3 + 4 = 7"
+
+var num1 = prompt("First number: ");
+var type = prompt("Operation : (+, -, /, *)");
+var num2 = prompt("Second number: ");
+
+switch(type){
+    case "+":
+        var total = add(num1,num2);
+        console.log(`${num1} ${type} ${num2} = ${total}`);
+    case "-":
+            var total = subtract(num1,num2);
+            console.log(`${num1} ${type} ${num2} = ${total}`);       
+    case "*":
+            var total = multiply(num1,num2);
+            console.log(`${num1} ${type} ${num2} = ${total}`); 
+    case "/":
+            var total = divide(num1,num2);
+            console.log(`${num1} ${type} ${num2} = ${total}`);}
